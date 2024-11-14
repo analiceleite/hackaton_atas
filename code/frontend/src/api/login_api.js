dfaa1e8c8f18573033992f39f7c8c0ceb28ac546
Analice <analice.leite12@gmail.com>
2024-11-14 14:32:49 -0300

    :sparkles: feat: Integração com o reconhecimento facial

--- code/frontend/src/api/login_api.js
+++ code/frontend/src/api/login_api.js
@@ -3,15 +3,17 @@ import { API_BASE_URL } from "./base_api";
 
 export const login = async (email, password) => {
     try {
-        const response = await axios.post(`${API_BASE_URL}/api/auth/login/`);
+        const response = await axios.get(`${API_BASE_URL}/api/v1/recognition/detect-face/`);
+        console.log(response)
         
-        if (response.data.access && response.data.refresh) {
+        if (response.data) {
             localStorage.setItem('user_token', response.data.access);
             localStorage.setItem('user_refresh_token', response.data.refresh);
             return response.data;
         } else {
             throw new Error("Tokens não encontrados na resposta");
         }
+
     } catch (error) {
         console.error("Erro ao fazer login:", error.response?.data || error.message);
         throw error;
